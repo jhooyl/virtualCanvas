4d1d670aa91864339bdb88e0b5ea26814be54554
amirakiri19 <amiraamarr51@gmail.com>
2026-04-04 18:53:23 +0200

    brokenGallery_floor

--- virtual-canvas/scenes/character_body_2d.gd
+++ virtual-canvas/scenes/character_body_2d.gd
@@ -3,28 +3,26 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
-	# 1. Get the direction from your keyboard
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	# 2. Movement Logic
 	if direction:
 		velocity = direction * SPEED
 		
-		# --- ANIMATION MAGIC STARTS HERE ---
-		# This tells her which way to look based on your keys
-		if direction.x > 0:
-			$AnimatedSprite2D.play("walk_right")
-		elif direction.x < 0:
-			$AnimatedSprite2D.play("walk_left")
-		elif direction.y > 0:
-			$AnimatedSprite2D.play("walk_down")
-		elif direction.y < 0:
-			$AnimatedSprite2D.play("walk_up")
-		# ------------------------------------
+		# Play the correct animation
+		if direction.x > 0: $AnimatedSprite2D.play("walk_right")
+		elif direction.x < 0: $AnimatedSprite2D.play("walk_left")
+		elif direction.y > 0: $AnimatedSprite2D.play("walk_down")
+		elif direction.y < 0: $AnimatedSprite2D.play("walk_up")
+
+		# --- RHYTHM LOGIC ---
+		# Only play the sound if the timer is stopped
+		if $StepTimer.is_stopped():
+			$StepSound.play()
+			$StepTimer.start() # This "waits" 0.4 seconds before the next tap
 		
 	else:
 		velocity = Vector2.ZERO
-		# --- STOP THE LEGS ---
-		$AnimatedSprite2D.stop() 
+		$AnimatedSprite2D.stop()
+		# We don't need to stop the sound here anymore, it will just finish its tap
 	
 	move_and_slide()
