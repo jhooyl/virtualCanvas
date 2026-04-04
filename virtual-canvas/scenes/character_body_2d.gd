7249f887766c6a8110bd5cfb53953399c2e6bae5
amirakiri19 <amiraamarr51@gmail.com>
2026-04-04 18:24:43 +0200

    avatar

--- virtual-canvas/scenes/character_body_2d.gd
+++ virtual-canvas/scenes/character_body_2d.gd
@@ -3,14 +3,28 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
-	# 1. Get the direction from your keyboard (Left, Right, Up, Down)
+	# 1. Get the direction from your keyboard
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	# 2. If you are pressing keys, move. If not, stop.
+	# 2. Movement Logic
 	if direction:
 		velocity = direction * SPEED
+		
+		# --- ANIMATION MAGIC STARTS HERE ---
+		# This tells her which way to look based on your keys
+		if direction.x > 0:
+			$AnimatedSprite2D.play("walk_right")
+		elif direction.x < 0:
+			$AnimatedSprite2D.play("walk_left")
+		elif direction.y > 0:
+			$AnimatedSprite2D.play("walk_down")
+		elif direction.y < 0:
+			$AnimatedSprite2D.play("walk_up")
+		# ------------------------------------
+		
 	else:
-		velocity = direction * 0
-
-	# 3. This actually makes the character move and hit walls
+		velocity = Vector2.ZERO
+		# --- STOP THE LEGS ---
+		$AnimatedSprite2D.stop() 
+	
 	move_and_slide()
