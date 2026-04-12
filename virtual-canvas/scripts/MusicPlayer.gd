5b8466620f3c1ac0decba0f52fe8b338c9107b42
AntsaRakoto <antsarakotomanga12@gmail.com>
2026-04-12 22:17:24 +0200

    Ajout de son dans gallery et broken gallery

--- virtual-canvas/scripts/MusicPlayer.gd
+++ virtual-canvas/scripts/MusicPlayer.gd
@@ -6,6 +6,15 @@ func _ready():
 	autoplay = true
 	bus = "Master"
 	play()
+
+func play_music(music_path: String):
+	# On vérifie si la musique demandée est déjà en train de jouer
+	if stream and stream.resource_path == music_path:
+		return 
+	
+	# Sinon, on charge la nouvelle et on la joue
+	stream = load(music_path)
+	play()
 	
 func _on_finished():
 	play() # Relance la musique quand elle se termine
